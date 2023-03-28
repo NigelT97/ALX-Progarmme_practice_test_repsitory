f1be5cfa4b667af65b4cdac8cec49d4d49705ff7
NigelT97 <NigelT97>
2023-03-28 17:47:57 +0000

    john

---
+++ c_folder/Exec1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	int x,y,z;
+	y=1;
+	z=1;
+	x=y+z;
+	printf("ansewer is stil %d\n",x);
+	return (0);
+}
---
+++ c_folder/project2/putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry into progrsmme
+ * print out piutchar
+ * Return: zero
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return 0;
+}
